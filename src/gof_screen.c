3b73e6eb540ce6c34906764d8b323f335f650893
dumrelu <dum.relu@gmail.com>
2014-05-19 20:40:35 +0300

    Added choosing engine creation mode + display more info

--- src/gof_screen.c
+++ src/gof_screen.c
@@ -15,7 +15,7 @@ GoF_Screen *screen_create(int x, int y,int height, int width,int entity_length)
 
 void screen_draw(GoF_Screen *screen, Graphics *g, Engine *engine)
 {	//TODO: refactor
-	int actual_en_length = screen->entity_length - 2, i, j;
+	int a_l = screen->entity_length - 2, i, j;
 
 	//Set background color
 	graphics_background(g, 41, 64, 60);
@@ -42,14 +42,36 @@ void screen_draw(GoF_Screen *screen, Graphics *g, Engine *engine)
 		graphics_draw_line(g, 0, start_y, screen->width * screen->entity_length, end_y);
 	}
 
-	//Set entity draw color
-	graphics_color(g, 0, 255, 0);
-
 	//Draw entities
-	for(i = screen->y; i < screen->height; i++)
-		for(j = screen->x; j < screen->width; j++)
-			if(!engine_is_free(engine, j, i))
-				graphics_draw_fill_rect(g, j*screen->entity_length+1, i*screen->entity_length+1, actual_en_length, actual_en_length);
+	int x, y;
+	int last_type = -1;
+	for(i = 0; i < screen->height; i++)
+		for(j = 0; j < screen->width; j++)
+		{
+			x = j+screen->x;
+			y = i+screen->y;
+
+			if(engine_is_correct(engine, x, y))
+			{
+				if(!engine_is_free(engine, x, y))
+				{
+					if(last_type != 1) {
+						graphics_color(g, 0, 255, 0);
+						last_type = 1;
+					}
+					graphics_draw_fill_rect(g, j*screen->entity_length+1, i*screen->entity_length+1, a_l, a_l);
+				}
+			}
+			else
+			{
+				if(last_type != 2) {
+					graphics_color(g, 255, 0, 0);
+					last_type = 2;
+				}
+				graphics_draw_fill_rect(g, j*screen->entity_length+1, i*screen->entity_length+1, a_l, a_l);
+			}
+		}
+			
 
 	//Text color
 	graphics_color(g, 0, 0, 255);
@@ -62,8 +84,9 @@ void screen_draw(GoF_Screen *screen, Graphics *g, Engine *engine)
 	sprintf(info, "Number of entities in queue: %d / %d", engine->n_queue, MAX_ENTITIES);
 	graphics_draw_string(g, screen->width * screen->entity_length + 10, 30, info);
 
-	//Flush graphics
-	graphics_flush(g);
+	sprintf(info, "Current FPS: %d", engine->ups);
+	graphics_color(g, 255, 236, 139);
+	graphics_draw_string(g, screen->width * screen->entity_length + 10, 50, info);
 }
 
 void screen_free(GoF_Screen *screen)
--- src/main.c
+++ src/main.c
@@ -2,19 +2,30 @@
 #include "engine.h"
 #include "gof_screen.h"
 #include <unistd.h>
+#include <time.h>
+#include <ctype.h>
+
+#define LEFT_ARROW 81
+#define UP_ARROW 82
+#define RIGHT_ARROW 83
+#define DOWN_ARROW 84
+
+Engine *prepare_game(char *save_name);
 
 int main(int argc, char *argv[])
 {	
+	char input = 0;
+	char save_name[50];
+
+
 	//Prepare engine
-	Engine *engine = engine_create(30, 30, 30);
-	//engine_save(engine, "glider.sav");
-	//Engine *engine = engine_load("glider.sav");
+	Engine *engine = prepare_game(save_name);
 
 	GoF_Screen *screen = screen_create(0, 0, 30, 30, 20);
 	Graphics g;
 	graphics_init(&g, 800, 900, "The Game of Life!");
 
-	char input = 0;
+	input = 0;
 	while(input != 'k')
 	{
 		screen_draw(screen, &g, engine);
@@ -26,41 +37,102 @@ int main(int argc, char *argv[])
 			engine_remove_entity(engine, g.last_x/screen->entity_length, g.last_y/screen->entity_length);
 	}
 
+	//Input
 	input = 0;
+	int sens = 5;
+
+	//Main game loop
 	while(input != 'q')
-	{
+	{	
+		//Draw on screen
 		screen_draw(screen, &g, engine);
 
-		sleep(1);
-
+		//Update Engine
 		engine_update(engine);
 		
-		if(graphics_check_event(&g))
+		//Process input
+		while(graphics_check_event(&g))
+		{
 			input = graphics_event(&g);
+			switch(input)
+			{
+				case 'p':
+					engine->ups++;
+					break;
+				case 'm':
+					if(engine->ups > 1)
+						engine->ups--;
+					break;
+				case '.':
+					sens++;
+					break;
+				case ',':
+					if(sens > 1)
+						sens--;
+					break;
+				case UP_ARROW:
+					screen->y -= sens;
+					break;
+				case DOWN_ARROW:
+					screen->y += sens;
+					break;
+				case LEFT_ARROW:
+					screen->x -= sens;
+					break;
+				case RIGHT_ARROW:
+					screen->x += sens;
+					break;
+			}
+		}
+
+		//Sleep
+		usleep((1000/engine->ups) * 1000);
 	}
+
+	//Close graphics
+	graphics_close(&g);
+
+	//Save game
+	printf("Saving game...\n");
+	engine_save(engine, save_name);
 
 	//Free mem
 	engine_free(engine);
 	screen_free(screen);
-	graphics_close(&g);
 
 	return 0;
 }
 
-//Engine *engine = engine_create(30, 30, 30);
+Engine *prepare_game(char *save_name)
+{
+	char input;
 	
-	/*
-	engine_add_entity(engine, entity_create(5+10, 3+10));
-	engine_add_entity(engine, entity_create(4+10, 4+10));
-	engine_add_entity(engine, entity_create(5+10, 4+10));
-	engine_add_entity(engine, entity_create(6+10, 4+10));
-	engine_add_entity(engine, entity_create(4+10, 5+10));
-	engine_add_entity(engine, entity_create(6+10, 5+10));
-	engine_add_entity(engine, entity_create(5+10, 6+10));
-	
+	//Read option
+	printf("Load save? [Y/n]: ");
+	scanf("%c", &input);
 
-	engine_add_entity(engine, entity_create(15, 15));
-	engine_add_entity(engine, entity_create(16, 16));
-	engine_add_entity(engine, entity_create(16, 17));
-	engine_add_entity(engine, entity_create(15, 17));
-	engine_add_entity(engine, entity_create(14, 17));*/
+	//Process
+	if(toupper(input) == 'Y')
+	{
+		printf("Name of the save file to be loaded: ");
+		scanf("%s", save_name);
+		return engine_load(save_name);
+	} else
+	{
+		printf("Name of the new save file: ");
+		scanf("%s", save_name);
+		
+		int height, width, ups;
+		
+		printf("New engine:\n\t->height: ");
+		scanf("%d", &height);
+
+		printf("\t->width: ");
+		scanf("%d", &width);
+
+		printf("\t->ups: ");
+		scanf("%d", &ups);
+
+		return engine_create(height, width, ups);
+	}
+}
